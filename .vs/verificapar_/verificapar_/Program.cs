dba41f0a166aa7803fba46ed87dbbfaf9d5597d2
wendersonqsantos <wendersonqsantos@gmail.com>
2021-10-22 15:30:19 -0300

    Fibonacci

---
+++ .vs/verificapar_/verificapar_/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace verificapar_
+{
+    class Program
+    {
+        public static int recebe_numero(int numero)//recebe um valor inteiro e o retorna
+        {
+            Console.WriteLine("Digite um Valor Inteiro:");
+            numero = int.Parse(Console.ReadLine());
+            return numero;
+        }
+        public static void verificapar(int num) //verifica se o numero informado é par ou impar
+        {
+            if (num % 2 == 0)
+            {
+                imprime(num);
+                Console.Write(" é par");
+            }
+            imprime(num);
+            Console.Write(" é impar");
+        }
+        public static void imprime(int num)
+        {
+            Console.WriteLine("o numero Numero: "+num);
+        }
+        static void Main(string[] args)
+        {
+            int numero=0;
+            numero=recebe_numero(numero);
+            verificapar(numero);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+       public static List<int> calc(List<int> fib, int quant)//calculo sequencia fibonacci
+        {
+            int auxprev = 1;
+            int auxnext=0;
+            fib.Add(auxprev);
+            while (fib.Count != quant+1)
+            {
+                if (fib.Count <2)
+                {
+                    auxnext = auxprev;
+                  fib.Add(auxnext);
+                }
+                else
+                {
+                    auxnext += auxprev;
+                    fib.Add(auxnext);
+                    auxprev = auxnext-auxprev;
+                }                    
+                
+            }
+            return fib;
+        }
+        public static void sequence(List<int> fib)
+        {
+            int quant=0;
+            Console.WriteLine("Informe a quantidade de sequencia de Fibonacci:");
+            quant = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sequencia de Fibonacci: ");
+            imprime(calc(fib,quant));
+            
+        }
+        public static  void imprime(List<int> fib)
+        {
+            foreach(var valor in fib)
+            {
+                Console.Write(" "+valor);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int quantidade=0;
+            List<int> _fibonacci=new List<int>();
+             sequence(_fibonacci);
+            Console.ReadKey();
+
+        }
+    }
+}
